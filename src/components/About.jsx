66d7e167c53b85343bc12511aa7e7f6939e01c78
Sourabh Upadhye <sourabh.1998upadhye@gmail.com>
2023-01-18 18:28:47 +0530

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ function About(props) {
                     {parseIntro(data.about)}
                   </Col>
                   <Col style={styles.introImageContainer}>
-                    <img src={data?.imageSource} alt="profile" />
+                    <img src={data?.imageSource} style={{height:"380px"}} alt="profile" />
                   </Col>
                 </Row>
               </Fade>
